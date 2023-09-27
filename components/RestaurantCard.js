06fba8fb9a28ecd6816a9e2086ff4c3bf6f7876d
Mateus Abu Kamel <88752743+MateusAbu@users.noreply.github.com>
2023-09-27 11:34:00 -0300

    Restaurant Screen

--- components/RestaurantCard.js
+++ components/RestaurantCard.js
@@ -3,6 +3,7 @@ import React from 'react'
 import { StarIcon } from 'react-native-heroicons/solid'
 import { MapPinIcon } from 'react-native-heroicons/outline'
 import { urlFor } from '../sanity'
+import { useNavigation } from '@react-navigation/native'
 
 const RestaurantCard = ({
     id,
@@ -16,8 +17,26 @@ const RestaurantCard = ({
     long,
     lat,
 }) => {
+
+    const navigation = useNavigation()
+
     return (
-        <TouchableOpacity className='bg-white mr-3 shadow'>
+        <TouchableOpacity
+            onPress={() => {
+                navigation.navigate('Restaurant', {
+                    id,
+                    imgUrl,
+                    title,
+                    rating,
+                    genre,
+                    address,
+                    short_description,
+                    dishes,
+                    long,
+                    lat,
+                })
+            }}
+            className='bg-white mr-3 shadow'>
             <Image
                 source={{
                     uri: urlFor(imgUrl).url()
@@ -31,13 +50,13 @@ const RestaurantCard = ({
                 <View className='flex-row items-center space-x-1'>
                     <StarIcon color='green' opacity={0.5} size={22} />
                     <Text className='text-xs text-gray-500'>
-                        <Text className='text-green-500'>{rating}</Text> . {genre}
+                        <Text className='text-green-500'>{rating}</Text> {genre}
                     </Text>
                 </View>
 
                 <View className='flex-row items-center space-x-1'>
                     <MapPinIcon color='gray' opacity={0.4} size={22} />
-                    <Text className='text-xs text-gray-500'>Nearby . {address}</Text>
+                    <Text className='text-xs text-gray-500'>Nearby - {address}</Text>
                 </View>
 
             </View>
